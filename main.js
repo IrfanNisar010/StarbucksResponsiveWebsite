50a49076f16ec60990e2f7c0dd3dfa834b3c449a
Irfan Nisar <inzr010@gmail.com>
2024-01-10 19:03:32 +0530

    Upadate

---
+++ main.js
@@ -0,0 +1,34 @@
+
+//signup button
+document.getElementById("signup").addEventListener("click",function(){
+    window.location.href="https://www.starbucks.com/account/signin?ReturnUrl=%2F"
+}
+)
+//join button
+document.getElementById("join").addEventListener("click",function(){
+    window.location.href="https://www.starbucks.com/account/create"
+}
+)
+
+//explore the menu
+document.getElementById("explore-main").addEventListener("click",function(){
+    window.location.href="https://www.starbucks.com/account/create"
+}
+)
+
+//order now1
+document.getElementById("order-main1").addEventListener("click",function(){
+    window.location.href="https://www.starbucks.com/menu/drinks/cold-drinks"
+}
+)
+//order now2
+document.getElementById("order-main2").addEventListener("click",function(){
+    window.location.href="https://www.starbucks.com/menu/product/2123644/iced"
+}
+)
+
+//order now3
+document.getElementById("order-main3").addEventListener("click",function(){
+    window.location.href="https://www.starbucks.com/menu/product/2123236/single"
+}
+)
